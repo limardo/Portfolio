775ff7d01e44dff8afefa816abbd105467bb820b
Luca Limardo <lucalimardo@yahoo.it>
2015-01-05 12:06:19 +0100

    Update 2015-01-05 12:06

--- core/error/inline.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <?php include APP_PATH . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'error' . DIRECTORY_SEPARATOR . 'style.php'; ?>
-    <?php echo self::$_error_output; ?>
-</div>
